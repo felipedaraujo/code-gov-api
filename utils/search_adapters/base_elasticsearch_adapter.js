51dd1b629ec58bdaf747fd80f4398f2d145f8fae
Froilan Irizarry <froilan@froilanirizarry.me>
2018-05-30 10:40:21 -0700

    Remove dead code

--- utils/search_adapters/base_elasticsearch_adapter.js
+++ utils/search_adapters/base_elasticsearch_adapter.js
@@ -16,21 +16,6 @@ class BaseElasticsearchAdapter extends AbstractSearchAdapter {
     super();
     this.config = config;
   }
-
-  //TODO: We might be able to eliminate all this code and the AbstractSearchAdapter. Something to think about
-  getHostsFromConfig() {
-    let hosts = [];
-
-    if (Array.isArray(this.config.ES_HOST)) {
-      this.config.ES_HOST.forEach(host => {
-        hosts.push(`${host}:${this.config.ES_PORT}`);
-      });
-    } else {
-      hosts.push(`${this.config.ES_HOST}:${this.config.ES_PORT}`);
-    }
-
-    return hosts;
-  }
 }
 
 module.exports = BaseElasticsearchAdapter;
